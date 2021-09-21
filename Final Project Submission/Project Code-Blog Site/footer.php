c0bbaaf3473396713f53931cce4c57dd902aecba
Sheikh Afrin <86251883+SheikhAfrin@users.noreply.github.com>
2021-09-21 13:37:49 +0600

    Add files via upload

---
+++ Final Project Submission/Project Code-Blog Site/footer.php
@@ -0,0 +1,47 @@
+<!--footer-->
+	<footer>
+		<div class="container">
+			
+			<!-- footer -->
+			<div class="footer-cpy text-center">
+				<div class="footer-social">
+					<div class="copyrighttop">
+						<ul>
+							<li class="mx-3">
+								<a class="facebook" href="<?php getlinks("links","facebook");?>">
+									<i class="fab fa-facebook-f"></i>
+									<span>Facebook</span>
+								</a>
+							</li>
+							<li>
+								<a class="facebook" href="<?php getlinks("links","twitter");?>">
+									<i class="fab fa-twitter"></i>
+									<span>Twitter</span>
+								</a>
+							</li>
+							<li class="mx-3">
+								<a class="facebook" href="<?php getlinks("links","googleplus");?>">
+									<i class="fab fa-google-plus-g"></i>
+									<span>Google+</span>
+								</a>
+							</li>
+							<li>
+								<a class="facebook" href="<?php getlinks("links","pinterest");?>">
+									<i class="fab fa-pinterest-p"></i>
+									<span>Pinterest</span>
+								</a>
+							</li>
+						</ul>
+
+					</div>
+				</div>
+				<div class="w3layouts-agile-copyrightbottom">
+					<p> E-commerce Project By Afrin
+						
+					</p>
+
+				</div>
+			</div>
+			<!-- //footer -->
+		</div>
+	</footer>
